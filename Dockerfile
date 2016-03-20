042fc342c399bf9ce37c007053862859012b938a
Emory Merryman <emory.merryman+2N61ZmeAFB9TKDIP@gmail.com>
2016-03-20 12:21:32 +0000

    added service to configure jenkins using cli

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM taf7lwappqystqp4u7wjsqkdc7dquw/grimdog_systemd
-RUN systemctl disable dnf-makecache.timer
 RUN dnf install --assumeyes jenkins && dnf clean all && systemctl enable jenkins.service
 COPY src/root/usr/lib/systemd/system/bigneptune.timer /usr/lib/systemd/system
 COPY src/root/usr/lib/systemd/system/bigneptune.service /usr/lib/systemd/system
 COPY src/root/usr/local/sbin/bigneptune.sh /usr/local/sbin
 RUN chmod 0500 /usr/local/sbin/bigneptune.sh
-RUN systemctl enable bigneptune.timer
+RUN systemctl enable bigneptune.service
 EXPOSE 8080
 CMD ["/usr/sbin/init"]
--- src/root/usr/local/sbin/bigneptune.sh
+++ src/root/usr/local/sbin/bigneptune.sh
@@ -2,9 +2,14 @@
 
 cd $(mktemp -d) &&
     curl http://www.java2s.com/Code/JarDownload/localizer/localizer-1.9.jar.zip > localizer-1.9.jar.zip &&
-    unzip localizaer-1.9.jar.zip &&
+    unzip localizer-1.9.jar.zip &&
     curl http://central.maven.org/maven2/commons-codec/commons-codec/1.9/commons-codec-1.9.jar > commons-code-1.9.jar &&
     export CLASSPATH=/usr/share/jenkins/webroot/WEB-INF/jenkins-cli.jar:/usr/share/jenkins/webroot/WEB-INF/remoting.jar:/usr/share/jenkins/webroot/WEB-INF/slave.jar:/usr/share/jenkins/webroot/WEB-INF/classes:localizer-1.9.jar:commons-code-1.9.jar:localizer-1.9.jar &&
+    while ! java hudson.cli.CLI -s http://localhost:8080 help
+    do
+	sleep 1m &&
+	    true
+    done &&
     java hudson.cli.CLI -s http://localhost:8080 install-plugin greenballs &&
     java hudson.cli.CLI -s http://localhost:8080 safe-restart &&
     true
