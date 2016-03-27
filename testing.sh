7067877eccfa176b2fb4d83d5a4e90cf60a97eb3
Emory Merryman <emory.merryman+2N61ZmeAFB9TKDIP@gmail.com>
2016-03-27 19:33:49 +0000

    fixing testing script

--- testing.sh
+++ testing.sh
@@ -2,7 +2,7 @@
 
 npm install &&
     IMAGE=taf7lwappqystqp4u7wjsqkdc7dquw/stormyhook_jenkins_$(date +s) &&
-    docker build -t ${IMAGE} &&
+    docker build -t ${IMAGE} . &&
     docker run -d --privileged -ti -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 8081:8080 ${IMAGE} &&
     ./node_modules/.bin/protractor protractor.conf.js &&
     true
