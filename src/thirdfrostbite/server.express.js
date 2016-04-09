0ff32d57d4e9d28b00b60e08c3765d8b0c4bcc28
Emory Merryman <emory.merryman+ZimkAJ0FblRBcDOa@gmail.com>
2016-04-09 17:12:50 +0000

    fixed comma

--- src/thirdfrostbite/server.express.js
+++ src/thirdfrostbite/server.express.js
@@ -18,6 +18,8 @@
 		    "/usr/share/jenkins/webroot/WEB-INF/classes:localizer-1.9.jar"
 		].join(":"),
 		"hudson.cli.CLI",
+		"-s",
+		"http://localhost:8080",
 		"help"
 	    ]).then(function(success){
 		return response.json({
