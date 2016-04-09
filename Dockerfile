3957449266d5d0c8666c03de11b7fc27f7e4fbc4
Emory Merryman <emory.merryman+ZimkAJ0FblRBcDOa@gmail.com>
2016-04-09 13:52:32 +0000

    add help

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM taf7lwappqystqp4u7wjsqkdc7dquw/grimdog_systemd
 RUN dnf install --assumeyes jenkins && dnf clean all && systemctl enable jenkins.service
-RUN dnf install --assumeyes npm nodejs
+RUN dnf install --assumeyes npm nodejs curl
 RUN mkdir /opt/thirdfrostbite
 COPY src/thirdfrostbite/package.json /opt/thirdfrostbite
 COPY src/thirdfrostbite/server.express.js /opt/thirdfrostbite
 COPY src/thirdfrostbite/thirdfrostbite.service /usr/lib/systemd/system
-RUN cd /opt/thirdfrostbite && npm install
+RUN cd /opt/thirdfrostbite && npm install && curl -o /opt/thirdfrostbite/localizer-1.9.jar.zip http://www.java2s.com/Code/JarDownload/localizer/localizer-1.9.jar.zip && unzip -d /opt/thirdfrostbite /opt/thirdfrostbite/localizer-1.9.jar.zip && curl -o commons-codec-1.9.jar http://central.maven.org/maven2/commons-codec/commons-codec/1.9/commons-codec-1.9.jar 
 RUN systemctl enable thirdfrostbite.service
 EXPOSE 8080
 EXPOSE 29615
--- src/thirdfrostbite/server.express.js
+++ src/thirdfrostbite/server.express.js
@@ -1,4 +1,4 @@
-(function(express){
+(function(express, spawn){
     "use strict";
     return express()
 	.get("/rest/api/0.0.0/status/health", function(request, response){
@@ -6,7 +6,27 @@
 		status: true
 	    });
 	})
+	.post("/rest/api/0.0.0/jenkins-cli/help", function(request, response){
+	    return spawn("java", [
+		"-classpath",
+		[
+		    "/opt/thirdfrostbite/localizer-1.9.jar",
+		    "/opt/thirdfrostbite/commons-codec-1.9.jar"
+		    "/usr/share/jenkins/webroot/WEB-INF/jenkins-cli.jar",
+		    "/usr/share/jenkins/webroot/WEB-INF/remoting.jar",
+		    "/usr/share/jenkins/webroot/WEB-INF/slave.jar",
+		    "/usr/share/jenkins/webroot/WEB-INF/classes:localizer-1.9.jar"
+		].join(":"),
+		"hudson.cli.CLI"
+				 ]).then(function(success){
+				     return response.json({
+					 status: true,
+					 stdout: success.stdout,
+					 stderr: success.stderr
+				     })
+				 });
+	})
 	.listen(29615)
 }(
-    require("express")
+    require("express"),
 ));
