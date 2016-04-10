7fde002f166e313f75b673d9a0dc156e7ab243b7
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-10 15:19:03 -0400

    simplify generic help

--- src/thirdfrostbite/server/server.express.js
+++ src/thirdfrostbite/server/server.express.js
@@ -1,5 +1,28 @@
 (function(express, spawn, join){
     "use strict";
+    var error = function(rageguy){
+	return response.json({
+	    status: false,
+	    error: JSON.stringify(rageguy)
+	});
+    };
+    var run = function(){
+	return spawn([
+	    "java",
+	    "-classpath",
+	    [
+		"/opt/thirdfrostbite/localizer-1.9.jar",
+		"/opt/thirdfrostbite/commons-codec-1.9.jar",
+		"/usr/share/jenkins/webroot/WEB-INF/jenkins-cli.jar",
+		"/usr/share/jenkins/webroot/WEB-INF/remoting.jar",
+		"/usr/share/jenkins/webroot/WEB-INF/slave.jar",
+		"/usr/share/jenkins/webroot/WEB-INF/classes:localizer-1.9.jar"
+	    ].join(":"),
+	    "hudson.cli.CLI",
+	    "-s",
+	    "http://localhost:8080"
+	].concat(arguments).join(" "));
+    };
     return express()
 	.use("/", express.static(join(__dirname,"..","client")))
 	.get("/rest/api/0.0.0/status/health", function(request, response){
@@ -8,38 +31,20 @@
 	    });
 	})
 	.get("/rest/api/0.0.0/jenkins-cli/genericHelp", function(request, response){
-	    return spawn([
-		"java",
-		"-classpath",
-		[
-		    "/opt/thirdfrostbite/localizer-1.9.jar",
-		    "/opt/thirdfrostbite/commons-codec-1.9.jar",
-		    "/usr/share/jenkins/webroot/WEB-INF/jenkins-cli.jar",
-		    "/usr/share/jenkins/webroot/WEB-INF/remoting.jar",
-		    "/usr/share/jenkins/webroot/WEB-INF/slave.jar",
-		    "/usr/share/jenkins/webroot/WEB-INF/classes:localizer-1.9.jar"
-		].join(":"),
-		"hudson.cli.CLI",
-		"-s",
-		"http://localhost:8080",
-		"help"
-	    ].join(" ")).then(function(success){
+	    return run("help").then(function(success){
 		return response.json({
 		    status: true,
-		    response: success.stderr.split("\n").map(function(line, index, array){
-			if(0 === index % 2){
-			    return [line.trim(), array[index+1].trim()];
-			}
-		    }).filter(function(line, index, array){
-			return 0 === index % 2;
-		    })
+		    response: success.stderr
 		});
-	    }, function(rageguy){
+	    }, error);
+	})
+	.get("specificHelp:proc", function(request, response){
+	    return run("help", request.params.proc).then(function(success){
 		return response.json({
-		    status: false,
-		    error: JSON.stringify(rageguy)
+		    status: true,
+		    response: JSON.stringify(success)
 		});
-	    });
+	    }, error);
 	})
 	.listen(29615)
     ;
